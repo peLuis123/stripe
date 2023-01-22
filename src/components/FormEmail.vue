df77a17d7679be32ceeebea5dc0c4b8ac7275b3c
Pedro Luis Ramos Calla <86934125+peLuis123@users.noreply.github.com>
2023-01-22 16:13:42 -0500

    first commit

---
+++ src/components/FormEmail.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card width="500" >
+    <v-card-text>
+      <v-text-field v-model="email" dense label="Ingresar Correo" outlined>
+      </v-text-field>
+      <v-btn color="primary">Confirmar Correo</v-btn>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      email: ''
+    }
+  },
+  methods: {
+    name () {}
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <v-app-bar elevation="4" color="#00C853">
+        <v-toolbar-title> Correo@Correo.com </v-toolbar-title>
+        <v-spacer />
+        <v-btn elevation="0" color="#00C853">Market-Place</v-btn>
+        <v-btn elevation="0" color="#00C853">panel</v-btn>
+      </v-app-bar>
+    </div>
+    <div class="d-flex justify-center text-center mt-6">
+      <FormEmail />
+    </div>
+    <div >
+      <FormCheckout/>
+
+    </div>
+    <div>
+      paso 3
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FormEmail from '../components/FormEmail'
+import FormCheckout from '../components/FormCheckout'
 
 export default {
-  name: 'HomeView',
+  name: 'HomeApp',
+
   components: {
-    HelloWorld
+    FormEmail,
+    FormCheckout
   }
 }
 </script>
